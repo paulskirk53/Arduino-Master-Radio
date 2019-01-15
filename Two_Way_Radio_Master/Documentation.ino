463f3eb2ba5438793b89eaa23d6b9b628abc94d3
Paul <paulskirk53@gmail.com>
2019-01-15 12:10:59 +0000

    started to add LCD writes

--- Two_Way_Radio_Master/Documentation.ino
+++ Two_Way_Radio_Master/Documentation.ino
@@ -1,10 +1,6 @@
 // 17-11-2018
-// This sketch is uploaded to an arduino board connected to the observatory PC by a USB cable.
-// The arduino communicates with two other arduino boards using the NRF24l01+ transceivers
-// modelled on all three boards.
-// This skecth requests actions from the arduino shutter board (open, close, status)
+
 // It reuests the azimuth angle and status from the encoder arduino.
 
 
 //The sketch will eventually be maintaine in github - https://github.com/paulskirk53/Arduino-Master-Radio
-
--- Two_Way_Radio_Master/Two_Way_Radio_Master.ino
+++ Two_Way_Radio_Master/Two_Way_Radio_Master.ino
@@ -1,5 +1,6 @@
+
 //this is the MASTER reviewed and checked for process on Sunday 2-12 @12.37 see below
-// needs stable power supply
+// needs some error checks  and perhaps printing to an LCD with two rows for sent and two for received
 //bug 2 hashes added to command -
 // sequence needs to be this for master
 /*
@@ -18,38 +19,38 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
 #include <printf.h>
 
 #define PIN10  10
 
-RF24 radio(7, 8);                                 // CE, CSN
-const byte Encoder_address[6] = "encod";          //the address used to write to the encoder arduino board
-const byte Shutter_address[6] = "shutt";          //the address used to write to the shutter arduino board
+RF24 radio(7, 8);                            // CE, CSN
+const byte Encoder_address[6] = "encod";     // the address used to write to the encoder arduino board
+const byte Shutter_address[6] = "shutt";     // the address used to write to the shutter arduino board
 const byte Master_address[6] = "mastr";
 String  ReceivedData  = "";
 String Message = "";
 String stringtosend;
 
 bool tx_sent;
-char theCommand[32] = "";                   // confusingly, you can initialise a char array in this way, but later in code, it is not possible to assign in this way.
+char theCommand[32] = "";                    // confusingly, you can initialise a char array in this way, but later in code, it is not possible to assign in this way.
 double ReceivedPayload = 0.0;
 char response[32] = "";
 
 void setup()
 {
-  //pinMode(PIN10, OUTPUT);      // this is an NRF24L01 requirement if pin 10 is not used
-  Serial.begin(9600);            // this module uses the serial channel to Tx/ Rx commands from the Dome driver
+  //pinMode(PIN10, OUTPUT);                   // this is an NRF24L01 requirement if pin 10 is not used
+  Serial.begin(9600);                         // this module uses the serial channel to Tx/ Rx commands from the Dome driver
   printf_begin();
 
   radio.begin();
-  radio.setChannel(100); //ensure it matches the target host causes sketch to hang
+  radio.setChannel(100);                     // ensure it matches the target host causes sketch to hang
   radio.enableAckPayload();
-  radio.setDataRate(RF24_250KBPS);  // set RF datarate didn't work with the + devices either
+  radio.setDataRate(RF24_250KBPS);           // set RF datarate 
   radio.setPALevel(RF24_PA_LOW);
-  radio.setRetries(15, 15);           // time between tries and No of tries
-  radio.enableDynamicPayloads();     // needs this for acknowledge to work
-  radio.openReadingPipe(1, Master_address);     //NEED 1 for shared addresses
+  radio.setRetries(15, 15);                  // time between tries and No of tries
+  radio.enableDynamicPayloads();             // needs this for acknowledge to work
+  radio.openReadingPipe(1, Master_address);  //NEED 1 for shared addresses
 
 }
 
@@ -67,11 +68,10 @@ void loop()
 */
   initialisethecommand_to_null();
 
-  if (Serial.available() > 0)         // the dome driver has sent a command
+  if (Serial.available() > 0)                      // the dome driver has sent a command
   {
-    // Serial.print("Radio channel used is ");
-    // Serial.println(radio.getChannel());
-    ReceivedData = Serial.readStringUntil('#');            // the string does not contain the # character
+   
+    ReceivedData = Serial.readStringUntil('#');   // the string does not contain the # character
 
     //ReceivedData.equalsIgnoreCase("AZ")
     if ((ReceivedData.equalsIgnoreCase("AZ")) || (ReceivedData.equalsIgnoreCase("SA")) || (ReceivedData.equalsIgnoreCase("SL")))
@@ -93,22 +93,22 @@ void loop()
     }  //endif received AZ
 
 
-    if (ReceivedData.equalsIgnoreCase("OS"))   // fill this area for open shutter
+    if (ReceivedData.equalsIgnoreCase("OS"))           // open shutter
     {
-      // radio.stopListening();                     // need this before openwriting pipe - i think it i before radio.write
+      // radio.stopListening();                        // need this before openwriting pipe 
       radio.openWritingPipe(Shutter_address);
-      theCommand[0] = 'O';                                // note single quote use
+      theCommand[0] = 'O';                             // note single quote use
       theCommand[1] = 'S';
       theCommand[2] = '#';
       SendTheCommand();         // this command works as part of the 2 way
     }
 
 
-    if (ReceivedData.equalsIgnoreCase("CS"))   // fill this area for close shutter
+    if (ReceivedData.equalsIgnoreCase("CS"))           //  close shutter
     {
-      // radio.stopListening();                     // need this before openwriting pipe - i think it i before radio.write
+      // radio.stopListening();                        //  need this before openwriting pipe 
       radio.openWritingPipe(Shutter_address);
-      theCommand[0] = 'C';                                // note single quote use
+      theCommand[0] = 'C';                             //  note single quote use
       theCommand[1] = 'S';
       theCommand[2] = '#';
       SendTheCommand();
@@ -116,14 +116,14 @@ void loop()
 
 
 
-    if (ReceivedData.equalsIgnoreCase("SS"))   // fill this area for shutter status
+    if (ReceivedData.equalsIgnoreCase("SS"))          // shutter status
     {
-      // radio.stopListening();                    // need this before openwriting pipe - i think it i before radio.write
+      // radio.stopListening();                       // need this before openwriting pipe
       radio.openWritingPipe(Shutter_address);
-      theCommand[0] = 'S';                                // note single quote use
+      theCommand[0] = 'S';                            // note single quote use
       theCommand[1] = 'S';
       theCommand[2] = '#';
-      SendTheCommand();                                  // ok to here
+      SendTheCommand();                                
       ReceiveTheResponse();
       TransmitToDriver();
 
@@ -137,19 +137,22 @@ void loop()
 
 void initialisethecommand_to_null()
 {
-  for ( int i = 0; i < 32; i++)                //initialise the command array back to nulls
+  for ( int i = 0; i < 32; i++)                       // initialise the command array back to nulls
   {
     theCommand[i] = 0;
   }
-}
+} // end void initialisethecommand_to_null
+
 void SendTheCommand()
 {
+// write the command to the LCD here re-using receivedData to hold the converted char list ReceivedData is initialised a few lines on...
+// syntax - ReceivedData = String(theCommand); 
+
+
   radio.stopListening();
   tx_sent = radio.write(&theCommand, sizeof(theCommand));
 
-  //radio.openReadingPipe(1, Master_address);    //already opened in setup
-
-  radio.startListening();                         //put after the radio write so that no delay to start listening for response
+  radio.startListening();                            // put after the radio write so that no delay to start listening for response
 
   //Serial.print("The text sent was ");
   //Serial.println(theCommand);
@@ -163,13 +166,13 @@ void ReceiveTheResponse()
 {
   if (tx_sent)
   {
-    Serial.print("the command was...");
-    Serial.println(theCommand);
+   // Serial.print("the command was...");
+   // Serial.println(theCommand);
    
 
     unsigned long currentMillis;
     unsigned long prevMillis;
-    unsigned long txIntervalMillis = 300; // wait 0.3 seconds to see if a response will be received
+    unsigned long txIntervalMillis = 300;            // wait 0.3 seconds to see if a response will be received
     
     prevMillis = millis();
     while (!radio.available())
@@ -185,43 +188,44 @@ void ReceiveTheResponse()
       currentMillis = millis();
       if (currentMillis - prevMillis > txIntervalMillis)
       {
-        Serial.println("retry ");
-        Serial.println(currentMillis - prevMillis);
+       // Serial.println("retry ");
+       // Serial.println(currentMillis - prevMillis);
         SendTheCommand();
         prevMillis = currentMillis ; //reset the timer
       }
 
 
-    }                                           // end while
+    }  // end while
 
     if (radio.available())  // nrf radio is only available if the complete TX was successful - datasheet
     {
       radio.read(&response, sizeof(response));
       //radio.stopListening(); 
       
-      stringtosend = String(response);                  // convert char to string for sending to driver
+      stringtosend = String(response);               // convert char to string for sending to driver
     }
   }
   else
   {
     stringtosend = "";
-    Serial.println("[-] The transmission to the selected node failed.");
-    
+   // write this to LCD Serial.println("[-] The transmission to the selected node failed.");
+   // need to think about what to do if tx fails 
   }
 
 
 
-}
+} // end void receivetheresponse
+
 void TransmitToDriver()
 {
 
 
   // need to change response to string i.e char to string
   
-  Serial.println("The message sent to the driver was ");
+ // print this to LCD Serial.println("The message sent to the driver was ");
 
-  Serial.println (stringtosend); //print value to serial, for the driver
+  Serial.println (stringtosend);              // print value to serial, for the driver
   //Serial.println("#");                      // print the string terminator
-  Serial.println("--------------------------------------------------------");
+ // Serial.println("--------------------------------------------------------");
 
-}
+}   // end void transmit to driver
