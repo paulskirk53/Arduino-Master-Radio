869b1de571031b28df379b226fcf59c974a1f9d5
Paul <paulskirk53@gmail.com>
2021-12-02 23:06:53 +0000

    striping out the NRF radio what a load of complexity to do such a small job!

--- include/Two_Way_Radio_Master.h
+++ include/Two_Way_Radio_Master.h
@@ -6,19 +6,16 @@
 #include <LiquidCrystal.h>
 #include <printf.h>
 
-void initialisethecommand_to_null();
-void initialisetheresponse_to_null();
-void SendTheCommand();
+
 void ReceiveTheResponse() ;
-void TransmitToDriver();
+
 void lcdprint(int col, int row, String mess);
-void TransmissionFailure();
+
 bool validate_the_response(String receipt);
-void AZaction();
+
 void SSaction();
-void ConfigureRadio(String Address)       ;
-void TestforlostRadioConfiguration(String NodeAddress) ;
-void CheckifResponseReceived()  ;
-void RadioFailureCheck() ;
-void CheckforRadioAlwaysAvailableError();
+
+
+
+
 
