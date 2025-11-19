009f217b5218653ec8b0423afabed9dc5acbdb89
Paul <paulskirk53@gmail.com>
2025-11-19 22:53:19 +0000

    delay to allow serial setup before use

--- src/Two_Way_Radio_Master.cpp
+++ src/Two_Way_Radio_Master.cpp
@@ -54,7 +54,7 @@ void setup()
   ASCOM.begin(19200);    // this module uses the serial channel as ASCOM Tx/ Rx commands from the Dome driver
   Bluetooth.begin(9600); // the bluetooth HC05 devices are set as baud 9600, so it's important to match.
   lcdprint(0, 1, " MCU version " + pkversion);
-  //  delay(2000);                             // gives time to see the message on the LCD
+  delay(500);                             // serial & BT setup time
   lcdprint(0, 3, blank);
   lcdprint(0, 3, "Awaiting BT Receipt");
 
